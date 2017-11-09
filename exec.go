c4e99e67e8b47b5444c4a065ecaa1a2808bb1764
Jesse Donat <donatj@gmail.com>
2017-11-09 11:56:58 -0600

    Small refactor

--- exec.go
+++ exec.go
@@ -81,43 +81,46 @@ func (h *HookExec) Exec(timeout time.Duration) error {
 	defer h.HookServer.Unlock()
 
 	for _, f := range files {
-		cmd := exec.Command(f)
-
-		cmd.Stdout = os.Stdout
-		cmd.Stderr = os.Stderr
-
-		stdin, err := cmd.StdinPipe()
-		if err != nil {
-			multierror.Append(result, err)
-			continue
-		}
-		defer stdin.Close()
-
-		// io.Copy( cmd.StdinPipe
-		err = cmd.Start()
-		if err != nil {
-			multierror.Append(result, err)
-			continue
-		}
-
-		h.Data.Seek(0, 0)
-		io.Copy(stdin, h.Data)
-		stdin.Close()
-
-		timer := time.AfterFunc(timeout, func() {
-			cmd.Process.Kill()
-		})
-
-		err = cmd.Wait()
-		timer.Stop()
-
-		if err != nil {
-			multierror.Append(result, err)
-			continue
-		}
+		err := execFile(f, h, timeout)
+		multierror.Append(result, err)
 	}
 
 	return result
+}
+
+func execFile(f string, h *HookExec, timeout time.Duration) error {
+	cmd := exec.Command(f)
+
+	cmd.Stdout = os.Stdout
+	cmd.Stderr = os.Stderr
+
+	stdin, err := cmd.StdinPipe()
+	if err != nil {
+		return err
+	}
+	defer stdin.Close()
+
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	h.Data.Seek(0, 0)
+	io.Copy(stdin, h.Data)
+	stdin.Close()
+
+	timer := time.AfterFunc(timeout, func() {
+		cmd.Process.Kill()
+	})
+
+	err = cmd.Wait()
+	timer.Stop()
+
+	if err != nil {
+		return err
+	}
+
+	return nil
 }
 
 // todo: base this on OS
