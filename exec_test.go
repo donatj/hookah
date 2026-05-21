1675208e1c2710921319865c09b0a17a40714a53
Jesse Donat <donatj@gmail.com>
2026-05-21 16:57:25 -0500

    Update exec_test for new arguments

--- exec_test.go
+++ exec_test.go
@@ -125,7 +125,7 @@ func TestEnvPopulatedCorrectly(t *testing.T) {
 		Stdout: out,
 	}
 
-	err := h.Exec("user", "repo", "event", "action", 1*time.Minute, "FOO=BAR", "BAZ=QUX")
+	err := h.Exec("user", "repo", "event", "action", "test-delivery", 1*time.Minute, "FOO=BAR", "BAZ=QUX")
 	if err != nil {
 		t.Error(err)
 	}
